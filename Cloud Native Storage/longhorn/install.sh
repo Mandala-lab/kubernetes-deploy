d949b4d47d688ff5cf9170f038dac712e4caf630
Mandala <xiconz@qq.com>
2024-10-08 13:10:58 +0800

    chore: rename

---
+++ Cloud Native Storage/longhorn/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# 启用 POSIX 模式并设置严格的错误处理机制
+set -o posix errexit -o pipefail
+
+wget https://raw.githubusercontent.com/longhorn/longhorn/v1.6.0/deploy/longhorn.yaml
+kubectl apply -f longhorn.yaml
+
+
+kubectl patch svc longhorn-frontend -n longhorn-system -p '{"spec":{"type":"NodePort"}}'
--- higress/helm/2/install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# 启用 POSIX 模式并设置严格的错误处理机制
-set -o posix errexit -o pipefail
-
-helm repo add higress.io https://higress.io/helm-charts
-helm install higress -n higress-system higress.io/higress --create-namespace --render-subchart-notes
